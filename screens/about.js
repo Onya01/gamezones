9111e0c3909c712e5b0727438e56a12d560d4ad0
Uchenna <ushmanp@gmail.com>
2021-04-23 14:21:40 +0100

    gamezones

---
+++ screens/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
+
+export default function About() {
+ return (
+  <View sytle={globalStyles.container}>
+   <Text>About Screen</Text>
+  </View>
+ )
+}
